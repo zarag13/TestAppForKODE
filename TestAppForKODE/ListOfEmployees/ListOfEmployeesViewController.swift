277c773bdac69b9f32ce7a74818acf1db850f349
zarag13 <Ksdobnikov@icloud.com>
2024-03-13 21:01:36 +0200

    mini fix for customNavigationBar

--- TestAppForKODE/ListOfEmployees/ListOfEmployeesViewController.swift
+++ TestAppForKODE/ListOfEmployees/ListOfEmployeesViewController.swift
@@ -14,6 +14,29 @@ protocol ListOfEmployeesViewControllerProtocol: AnyObject {
 
 class ListOfEmployeesViewController : BaseController {
     var presenter: ListOfEmployeesPresenter?
+    let categoriesMenu = NavigationBarTableList()
+        
+}
+
+extension ListOfEmployeesViewController {
+    override func setupViews() {
+        
+    }
+    
+    override func setupLayoutViews() {
+        NSLayoutConstraint.activate([
+            categoriesMenu.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            categoriesMenu.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            categoriesMenu.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            categoriesMenu.heightAnchor.constraint(equalToConstant: 96)
+        ])
+    }
+    
+    override func configureAppearance() {
+        view.addView(categoriesMenu)
+        
+        view.backgroundColor = .white
+    }
 }
 
 extension ListOfEmployeesViewController: ListOfEmployeesViewControllerProtocol {
