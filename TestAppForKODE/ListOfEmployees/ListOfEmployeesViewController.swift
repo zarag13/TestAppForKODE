db7b8d83b238ff8e86f06c1113cf86e427fb3ba7
zarag13 <Ksdobnikov@icloud.com>
2024-03-14 14:55:32 +0200

    create tableView for ListOfEmployee & add navigationBar

--- TestAppForKODE/ListOfEmployees/ListOfEmployeesViewController.swift
+++ TestAppForKODE/ListOfEmployees/ListOfEmployeesViewController.swift
@@ -14,8 +14,9 @@ protocol ListOfEmployeesViewControllerProtocol: AnyObject {
 
 class ListOfEmployeesViewController : BaseController {
     var presenter: ListOfEmployeesPresenter?
+    
     let categoriesMenu = NavigationBarTableList()
-        
+    let tableListOfEmployees = ListOfEmployeesTableView()
 }
 
 extension ListOfEmployeesViewController {
@@ -28,13 +29,18 @@ extension ListOfEmployeesViewController {
             categoriesMenu.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             categoriesMenu.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             categoriesMenu.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            categoriesMenu.heightAnchor.constraint(equalToConstant: 96)
+            categoriesMenu.heightAnchor.constraint(equalToConstant: 96),
+            
+            tableListOfEmployees.topAnchor.constraint(equalTo: categoriesMenu.bottomAnchor, constant: 16),
+            tableListOfEmployees.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableListOfEmployees.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableListOfEmployees.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
     
     override func configureAppearance() {
         view.addView(categoriesMenu)
-        
+        view.addView(tableListOfEmployees)
         view.backgroundColor = .white
     }
 }
--- TestAppForKODE/ListOfEmployees/ListOfEmployeesViews/NavigationBar/NavigationBarTableList.swift
+++ TestAppForKODE/ListOfEmployees/ListOfEmployeesViews/NavigationBar/NavigationBarTableList.swift
@@ -14,16 +14,25 @@ class NavigationBarTableList: BaseView {
         searchBar.backgroundColor = .white
         searchBar.placeholder = "Ведите имя, тег, почту..."
         searchBar.searchBarStyle = .minimal
+        searchBar.searchTextField.backgroundColor = UIColor(hexString: "#F7F7F8")
+        searchBar.searchTextField.borderStyle = .roundedRect
         return searchBar
     }()
     
     let categoriesMenu = CategoriesCollectionView()
+    
+    let lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hexString: "#C3C3C6")
+        return view
+    }()
 }
 
 extension NavigationBarTableList {
     override func setupViews() {
         addView(searchBar)
         addView(categoriesMenu)
+        addView(lineView)
         categoriesMenu.categoriesDelegate = self
     }
     
@@ -37,7 +46,12 @@ extension NavigationBarTableList {
             categoriesMenu.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
             categoriesMenu.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             categoriesMenu.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            categoriesMenu.heightAnchor.constraint(equalToConstant: 44)
+            categoriesMenu.heightAnchor.constraint(equalToConstant: 44),
+            
+            lineView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            lineView.heightAnchor.constraint(equalToConstant: 0.5),
+            lineView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            lineView.trailingAnchor.constraint(equalTo: trailingAnchor),
         ])
     }
     
